9f935573971660055140d6260ffd57b0e3a22ba1
Gizem Çalışkan <gizemcaliskan24@gmail.com>
2024-08-08 08:45:36 +0300

    Add Entities

---
+++ Core/TradeAPI.Domain/Entities/Brand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TradeAPI.Domain.Common;
+
+namespace TradeAPI.Domain.Entities
+{
+    public class Brand:EntityBase
+    {
+        public Brand() { }
+        public Brand(string name)
+        {
+            Name = name;
+        }
+        public string Name { get; set; }
+    }
+}
